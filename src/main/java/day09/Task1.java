07ce1d19446cf27fd5665e64b604322a94cb7f66
Alexander Samolov <alexsamolov@gmail.com>
2023-12-09 23:36:26 +0100

    Day 09 part 2
    - easy peasy

--- src/main/java/day09/Task1.java
+++ src/main/java/day09/Task1.java
@@ -35,6 +35,13 @@ public class Task1 {
           .sum();
 
         log.info("Part 1 result from '{}': {}", file, result);
+
+        var result2 = lines.stream()
+          .map(this::parseSequence)
+          .mapToLong(this::calculatePrev)
+          .sum();
+
+        log.info("Part 2 result from '{}': {}", file, result2);
     }
 
     // recursion version
@@ -60,6 +67,31 @@ public class Task1 {
         }
         // recursion step
         return sequence[sequence.length - 1] + calculateNext(diff);
+    }
+
+    // recursion version
+    private long calculatePrev(long[] sequence) {
+        // just one number
+        if (sequence.length == 1) {
+            log.warn("reached end of sequence");
+            return sequence[0]; // return it
+        }
+
+        var prev = sequence[0];
+        var diff = new long[sequence.length - 1];
+        boolean allZeroes = true;
+        for (int i = 1; i < sequence.length; i++) {
+            var currentDiff = sequence[i] - prev;
+            prev = sequence[i];
+            diff[i - 1] = currentDiff;
+            allZeroes &= currentDiff == 0;
+        }
+        // exit if all zeroes, returning same sequence
+        if (allZeroes) {
+            return sequence[0];
+        }
+        // recursion step
+        return sequence[0] - calculatePrev(diff);
     }
 
     private long[] parseSequence(String line) {
